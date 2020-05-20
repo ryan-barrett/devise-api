3486ba0c92ca1dfdc50418c6ba5580f94c6fa840
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-05-19 19:30:30 -0700

    import graphqlHTTP in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const graphqlHTTP = require('express-graphql');
+import graphqlHTTP from 'express-graphql';
 
 interface graphqlConfig {
   schema: any;
