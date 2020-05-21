900a0f72363935f058c4ad8fdf7300283cced7d7
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-05-21 13:04:17 -0700

    update build script to be one step

--- bash/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# get rid of the container if it already exists
-if [[ "$(docker images -q myimage:sudoku 2> /dev/null)" == "" ]]; then
-  docker kill /sudoku
-  docker rm /sudoku
-fi
-
-docker run -d --name sudoku -p 8080:8080 ryanbarrett1/sudoku
--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@
 [ ! -d "./dist" ] && mkdir dist # create dist if it doesn't exist
 tsc # compile typescript to JS
 docker build -t ryanbarrett1/sudoku . # build docker image
+
+# get rid of the container if it already exists
+if [[ "$(docker images -q myimage:sudoku 2> /dev/null)" == "" ]]; then
+  docker kill /sudoku
+  docker rm /sudoku
+fi
+
+docker run -d --name sudoku -p 8080:8080 ryanbarrett1/sudoku # run in docker
