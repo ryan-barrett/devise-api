03f9f94b99dcaff24a00a9573eb7cbce387a3ea7
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-06-01 18:41:21 -0700

    utility to generate document IDs

---
+++ src/utils/generateId.ts
@@ -0,0 +1,10 @@
+import { uuid } from 'uuidv4';
+
+import { connection } from '../data.source/cb.connection';
+
+export const generateId = async (): Promise<string> => {
+  const response = await connection.count();
+  const { rows } = response;
+  // @ts-ignore
+  return uuid() + Object.values(rows[0])[0];
+};
