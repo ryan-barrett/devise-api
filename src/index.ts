54a2376e1904c664e0e50daf7c1632fed472b174
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-06-01 18:40:39 -0700

    start migrating to jira clone format

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+require('dotenv').config();
 import { Server } from './server';
 import schema from './graphql/schema';
 
-const sudokuServer = new Server(8080, schema());
+const port = process.env.PORT || "8080";
+const sudokuServer = new Server(port, schema());
 
 sudokuServer.start();
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,3 @@
 import bunyan from 'bunyan';
 
-export const logger: bunyan = bunyan.createLogger({ name: 'Sudoku' });
+export const logger: bunyan = bunyan.createLogger({ name: 'Devise' });
