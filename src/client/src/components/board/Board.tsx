125f6f2db518a67e80630f5da4f153cf6e23e64b
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-06-14 00:30:22 -0700

    create basic frontend board outline

---
+++ src/client/src/components/board/Board.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Ticket from '../ticket/Ticket';
+
+import './Board.css';
+
+function Board() {
+  return (
+    <div>
+      <h1>
+        Board Name
+      </h1>
+      <div id="board-container">
+        <div className="board-col">
+          <h3>To Do</h3>
+          <Ticket />
+          <Ticket />
+          <Ticket />
+        </div>
+        <div className="board-col">
+          <h3>In Progress</h3>
+          <Ticket />
+          <Ticket />
+          <Ticket />
+        </div>
+        <div className="board-col">
+          <h3>Done</h3>
+          <Ticket />
+          <Ticket />
+          <Ticket />
+          <Ticket />
+          <Ticket />
+          <Ticket />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
