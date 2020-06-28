7bcc15aeb001dbeae4a847f04c686d99cbc6d074
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-06-27 20:11:27 -0700

    update default couchbase url

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.PORT || "8080",
   "couchbaseConfig": {
-    couchbaseUrl: process.env.COUCHBASE_URL || '8091',
+    couchbaseUrl: process.env.COUCHBASE_URL || 'localhost:8091',
     'defaultBucket': process.env.DEFAULT_BUCKET || 'b',
     cbUsername: process.env.CB_USERNAME || '',
     cbPassword: process.env.CB_PASSWORD || '',
