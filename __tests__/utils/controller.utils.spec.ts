48de2b6fc5a1cc7dd9375c41faa2d6fd109f0930
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-06-28 22:21:33 -0700

    tests for utility files

---
+++ __tests__/utils/controller.utils.spec.ts
@@ -0,0 +1,62 @@
+import { validateBoards, validateUser } from '../../src/utils/controller.utils';
+import { BoardModel } from '../../src/models/board.model';
+import { UserModel } from '../../src/models/user.model';
+
+describe('controller utilities', () => {
+  describe('validateBoards', () => {
+    const boardFind = jest.fn(() => {
+      return new Promise(((resolve, reject) => resolve(1)));
+    });
+    // @ts-ignore
+    BoardModel.Find = boardFind;
+
+    it('should do a find operation once for each board id', async () => {
+      await validateBoards(['1', '2', '3']);
+      expect(boardFind.mock.calls.length).toBe(3);
+    });
+
+    it('should return true if boards are all found', async () => {
+      const isValid = await validateBoards(['1', '2', '3']);
+      expect(isValid).toBe(true);
+    });
+
+    it('should return false if not all boards are found', async () => {
+      const boardFind = jest.fn(() => {
+        return new Promise(((resolve, reject) => resolve(0)));
+      });
+      // @ts-ignore
+      BoardModel.Find = boardFind;
+      const isValid = await validateBoards(['1']);
+      expect(boardFind.mock.calls.length).toBe(1);
+      expect(isValid).toBe(false);
+    });
+  });
+
+  describe('validateUser', () => {
+    const userFind = jest.fn(() => {
+      return new Promise(((resolve, reject) => resolve({ data: 1 })));
+    });
+    // @ts-ignore
+    UserModel.Find = userFind;
+
+    it('should do a find operation on the user id', async () => {
+      await validateUser('1');
+      expect(userFind.mock.calls.length).toBe(1);
+    });
+
+    it('should return true if the user is found', async () => {
+      const isValid = await validateUser('1');
+      expect(isValid).toBe(true);
+    });
+
+    it('should return false if the user is not found', async () => {
+      // @ts-ignore
+      UserModel.Find = jest.fn(() => {
+        return new Promise(((resolve, reject) => resolve(undefined)));
+      });
+
+      const isValid = await validateUser(('1'));
+      expect(isValid).toBe(false);
+    });
+  });
+});
---
+++ __tests__/utils/generate.id.spec.ts
@@ -0,0 +1,20 @@
+import { generateId } from '../../src/utils/generate.id';
+import { connection } from '../../src/data.source/cb.connection';
+
+const count = jest.fn(() => {
+  return new Promise((resolve, reject) => {
+    resolve({ rows: [[1]] });
+  });
+});
+// @ts-ignore
+connection.count = count;
+
+describe('generateId', () => {
+  it('generate a valid UUID', async () => {
+    const id = await generateId();
+    const uuid = id.split('').slice(0, id.length - 1).join('');
+    const uuidV4Regex = /^[A-F\d]{8}-[A-F\d]{4}-4[A-F\d]{3}-[89AB][A-F\d]{3}-[A-F\d]{12}$/i;
+    const isValidV4UUID = (uuid: string) => uuidV4Regex.test(uuid);
+    expect(isValidV4UUID(uuid)).toBe(true);
+  });
+});
