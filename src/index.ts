318843dcaf8a0f4939a19505e96478179d502c9c
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-08-15 20:39:26 -0700

    add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,14 @@ import { logger } from './utils/logger';
 const port: string = config.get('port');
 const server = new Server(port, schema());
 
-server.start().then(() => logger.info('idle...'));
+async function shutDown() {
+  logger.info('\n shutting down');
+  await server.stop();
+  logger.info('done');
+}
+
+server.start().then(() => {
+  process.on('SIGTERM', shutDown);
+  process.on('SIGINT', shutDown);
+  logger.info('idle...');
+});
