d2549fc24ddaa6ea27d1e5e4ec89b773856fc9fe
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-09-09 00:44:07 -0700

    remove unused ecr script

--- bash/ecr_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 846799957903.dkr.ecr.us-west-2.amazonaws.com
-docker build -t devise_repo .
-docker tag devise_repo:latest 846799957903.dkr.ecr.us-west-2.amazonaws.com/devise_repo:latest
-docker push 846799957903.dkr.ecr.us-west-2.amazonaws.com/devise_repo:latest
